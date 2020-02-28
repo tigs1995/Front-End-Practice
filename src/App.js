257ab49d827894b5d463ce24a99b92ae2083c4ae
tigs1995 <tigsye@hotmail.com>
2020-02-28 11:57:51 +0000

    Tutorial followed to add in relevant js files

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import './App.css';
 import Display from './Components/Display';
 import Login from "./Components/Login";
+import { Provider } from "react-redux";
+import Store from "./Store";
 
 
 export default class App extends Component {
   render() {
     return (
+      <Provider Store={Store}>
       <Router>
         <Link to="/">Home</Link>
         <Route exact path="/" component={Login}></Route>
         <Route path="/loggedIn/:username" component={Display}></Route>
       </Router>
+      </Provider>
   );
   }
 
