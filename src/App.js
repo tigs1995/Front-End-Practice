4c22005e9ecc7ccfff6e934858ded27ede211484
tigs1995 <tigsye@hotmail.com>
2020-03-03 15:34:01 +0000

    Home page and search pages added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import './App.css';
-import HomePage from './Components/HomePage';
 import Login from "./Components/Login";
 import { Provider } from "react-redux";
 import store from "./Store";
@@ -10,39 +9,70 @@ import setAuthToken from "./Utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./Actions/authActions";
 import PrivateRoute from "./Components/PrivateRoutes/PrivateRoute";
 
-// Check for token to keep user logged in
-if (sessionStorage.jwtToken) {
-  // Set auth token header auth
-  const token = sessionStorage.jwtToken;
-  setAuthToken(token);
-  // Decode token and get user info and exp
-  const decoded = jwt_decode(token);
-  // Set user and isAuthenticated
-  store.dispatch(setCurrentUser(decoded));
-// Check for expired token
-  const currentTime = Date.now() / 1000; // to get in milliseconds
-  if (decoded.exp < currentTime) {
-    // Logout user
-    store.dispatch(logoutUser());
-    // Redirect to login
-    window.location.href = "./login";
-  }
-}
+import HomePage from './Components/HomePage';
+
+import VehicleSearch from "./Components/SearchPages/VehicleSearch";
+import LocationSearch from "./Components/SearchPages/LocationSearch";
+import CitizenSearch from "./Components/SearchPages/CitizenSearch";
+
+// import VehicleCitizen from "./Components/CitizenInfo/VehicleCitizen";
+// import AssociatesCitizen from "./Components/CitizenInfo/AssociatesCitizen";
+// import HomePageCitizen from "./Components/CitizenInfo/HomePageCitizen";
+// import FinancialsCitizen from "./Components/CitizenInfo/FinancialsCitizen";
+
+// import Location from "./Components/Map/Location";
+
+// // Check for token to keep user logged in
+// if (sessionStorage.jwtToken) {
+//   // Set auth token header auth
+//   const token = sessionStorage.jwtToken;
+//   setAuthToken(token);
+//   // Decode token and get user info and exp
+//   const decoded = jwt_decode(token);
+//   // Set user and isAuthenticated
+//   store.dispatch(setCurrentUser(decoded));
+// // Check for expired token
+//   const currentTime = Date.now() / 1000; // to get in milliseconds
+//   if (decoded.exp < currentTime) {
+//     // Logout user
+//     store.dispatch(logoutUser());
+//     // Redirect to login
+//     window.location.href = "./login";
+//   }
+// }
 
 
 export default class App extends Component {
   render() {
     return (
+      // <Provider store={store}>
+      //   <Router>
+      //     <Link to="/">Home</Link>
+      //     <Route exact path="/" component={Login}></Route>
+      //     {/* <Route path="/loggedIn/:username" component={HomePage}></Route> */}
+      //     <Switch>
+      //       <PrivateRoute exact path="/loggedIn" component={HomePage} />
+      //     </Switch>
+      //   </Router>
+      // </Provider>
+
       <Provider store={store}>
         <Router>
-          <Link to="/">Home</Link>
-          <Route exact path="/" component={Login}></Route>
-          {/* <Route path="/loggedIn/:username" component={HomePage}></Route> */}
-          <Switch>
-            <PrivateRoute exact path="/loggedIn" component={HomePage} />
-          </Switch>
+          <Route exact path="/" component={HomePage}></Route>
+          <Route path="/CitizenSearch" component={CitizenSearch}></Route>
+          <Route path="/LocationSearch" component={LocationSearch}></Route>
+          <Route path="/VehicleSearch" component={VehicleSearch}></Route>
+          {/* <Route path="/CitizenHome/:name" component={HomePageCitizen}></Route>
+          <Route path="/CitizenFinancials/:id" component={FinancialsCitizen}></Route>
+          <Route path="/CitizenAssociates/:id" component={AssociatesCitizen}></Route>
+          <Route path="/CitizenVehicles/:id" component={VehicleCitizen}></Route>
+          <Route path="/CitizenHome/:name" component={CitizenSearch}></Route>
+          <Route path="/CitizenHome/:name" component={CitizenSearch}></Route>
+          <Route path="/CitizenHome/:name" component={CitizenSearch}></Route>
+          <Route path="/Map/:search" component={Location}></Route> */}
         </Router>
       </Provider>
+
     );
   }
 
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -3,58 +3,66 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../Actions/authActions";
 
-class HomePage extends React.Component {
+export default class HomePage extends React.Component {
 
-    onLogoutClick = e => {
-        e.preventDefault();
-        this.props.logoutUser();
-    };
+    // onLogoutClick = e => {
+    //     e.preventDefault();
+    //     this.props.logoutUser();
+    // };
 
     render() {
-        const { user } = this.props.auth;
         return (
-            // <div>
-            //     Thank you for logging in!
-            //     Username  : {this.props.match.params.username}
-            // </div>
-            <div style={{ height: "75vh" }} className="container valign-wrapper">
-                <div className="row">
-                    <div className="col s12 center-align">
-                        <h4>
-                            <b>Hey there,</b> {user.name.split(" ")[0]}
-                            <p className="flow-text grey-text text-darken-1">
-                                You are logged into a full-stack{" "}
-                                <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
-              </p>
-                        </h4>
-                        <button
-                            style={{
-                                width: "150px",
-                                borderRadius: "3px",
-                                letterSpacing: "1.5px",
-                                marginTop: "1rem"
-                            }}
-                            onClick={this.onLogoutClick}
-                            className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                        >
-                            Logout
-            </button>
-                    </div>
-                </div>
-            </div>
+            <button>test button</button>
+          
         );
     }
 }
 
-HomePage.propTypes = {
-    logoutUser: PropTypes.func.isRequired,
-    auth: PropTypes.object.isRequired
-};
-const mapStateToProps = state => ({
-    auth: state.auth
-});
+// render() {
+    // const { user } = this.props.auth;
+    // return (
+        // <div>
+        //     Thank you for logging in!
+        //     Username  : {this.props.match.params.username}
+        // </div>
+//             <div style={{ height: "75vh" }} className="container valign-wrapper">
+//                 <div className="row">
+//                     <div className="col s12 center-align">
+//                         <h4>
+//                             <b>Hey there,</b> {user.name.split(" ")[0]}
+//                             <p className="flow-text grey-text text-darken-1">
+//                                 You are logged into a full-stack{" "}
+//                                 <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
+//               </p>
+//                         </h4>
+//                         <button
+//                             style={{
+//                                 width: "150px",
+//                                 borderRadius: "3px",
+//                                 letterSpacing: "1.5px",
+//                                 marginTop: "1rem"
+//                             }}
+//                             onClick={this.onLogoutClick}
+//                             className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+//                         >
+//                             Logout
+//             </button>
+//                     </div>
+//                 </div>
+//             </div>
+//     );
+// }
+// }
 
-export default connect(
-    mapStateToProps,
-    { logoutUser }
-)(HomePage);
+// HomePage.propTypes = {
+//     logoutUser: PropTypes.func.isRequired,
+//     auth: PropTypes.object.isRequired
+// };
+// const mapStateToProps = state => ({
+//     auth: state.auth
+// });
+
+// export default connect(
+//     mapStateToProps,
+//     { logoutUser }
+// )(HomePage);
