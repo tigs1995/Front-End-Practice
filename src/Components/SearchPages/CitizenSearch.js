73264cef01a3c0b5ef3d129953f06f0d9a8da925
tigs1995 <tigsye@hotmail.com>
2020-03-03 16:44:24 +0000

    Citizen search class outline created

---
+++ src/Components/SearchPages/CitizenSearch.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class CitizenSearch extends Component {
+    // constructor(props){
+
+    // }
+    // render(){
+    //     return(
+
+    //     )
+    // }
+}
