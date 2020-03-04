06a821e78db610eb4a99f7d771e61d7e97dcea98
alex-creating <alex-russo1996@hotmail.co.uk>
2020-03-04 09:17:35 +0000

    Changed location changer to history.push

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -2,7 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../Actions/authActions";
-import SearchButton from "./SearchButton";
 
 export default class HomePage extends React.Component {
 
@@ -14,9 +13,9 @@ export default class HomePage extends React.Component {
 
     render() {
         return (<div>
-            <button onClick={()=> window.location.href="./CitizenSearch"} name="Citizen">Citizen </button>
-            <button onClick={()=> window.location.href="./VehicleSearch"} name="Vehicle">Vehicle</button>
-            <button onClick={()=> window.location.href="./LocationSearch"} name="Location">Location</button>
+            <button onClick={()=> this.props.history.push("./CitizenSearch")} name="Citizen">Citizen </button>
+            <button onClick={()=> this.props.history.push("./VehicleSearch")} name="Vehicle">Vehicle</button>
+            <button onClick={()=> this.props.history.push("./LocationSearch")} name="Location">Location</button>
                 </div>
         );
     }
