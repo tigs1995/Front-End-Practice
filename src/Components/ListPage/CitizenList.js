4e18b2922694f0e09735eb25079b520b4bef0c63
tigs1995 <tigsye@hotmail.com>
2020-03-05 09:31:17 +0000

    Citizen list page imports for table removed

--- src/Components/ListPage/CitizenList.js
+++ src/Components/ListPage/CitizenList.js
@@ -1,30 +1,25 @@
 import React, { Component } from 'react';
-import TableHead from '../Table/TableHead';
-import TableBody from '../Table/TableBody';
 import axios from 'axios';
 import { CITIZEN_LIST, BASE_URL } from '../Constants';
-import { Card, ListItem } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 
 export default class CitizenList extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            forenames: '',
+            surname: '',
             citizenList: [
-                { "forenames": "Lizzie", "surname": "Cowell", "citizenID": "222AD" },
-                { "forenames": "Lizzie", "surname": "Cowell", "citizenID": "111A1" },
-                { "forenames": "Lizzie", "surname": "Cowell", "citizenID": "444FF" },
-                { "forenames": "Lizzie", "surname": "Cowell", "citizenID": "567RE" }
-
-
-            ],
-            headerList: ["citizenID"]
+            ]
         }
     }
 
     componentDidMount(props) {
-        const forenames = this.props.match.params.forenames;
-        const surname = this.props.match.params.surname;
-        axios.get(`${BASE_URL}${CITIZEN_LIST}`, { forenames: forenames, surname: surname }).then(response => {
+        this.setState({forenames: this.props.match.params.forenames});
+        this.setState({surname: this.props.match.params.surname});
+        // const forenames = this.props.match.params.forenames;
+        // const surname = this.props.match.params.surname;
+        axios.get(`${BASE_URL}${CITIZEN_LIST}`, { forenames: this.state.forenames, surname: this.state.surname }).then(response => {
             if (response.data.Error) {
                 console.log(response.data.Error);
             }
@@ -56,7 +51,8 @@ render() {
 
     return (
         <div id='cardList'>
-            <h4>Please choose a citizen to view more information:</h4>
+            <h4>You searched for: {this.state.forenames} {this.state.surname}</h4>
+            <p>Please choose a citizen for more information:</p>
             {this.state.citizenList.sort(this.compare).map(citizen =>
                 <Card border="primary" class='citizenCard'>
                     <Card.Body>
