aa22dc5c9c3ec2967ddd8c525fd3337b49f520c0
tigs1995 <tigsye@hotmail.com>
2020-03-05 09:00:28 +0000

    Citizen list changed to cards instead of table

--- src/Components/ListPage/CitizenList.js
+++ src/Components/ListPage/CitizenList.js
@@ -3,14 +3,19 @@ import TableHead from '../Table/TableHead';
 import TableBody from '../Table/TableBody';
 import axios from 'axios';
 import { CITIZEN_LIST, BASE_URL } from '../Constants';
+import { Card, ListItem } from "react-bootstrap";
 
 export default class CitizenList extends Component {
     constructor(props) {
         super(props);
         this.state = {
             citizenList: [
-                {"citizenID": "GDhf74" },
-                {"citizenID": "gfgfgfgf" }
+                { "forenames": "Lizzie", "surname": "Cowell", "citizenID": "222AD" },
+                { "forenames": "Lizzie", "surname": "Cowell", "citizenID": "111A1" },
+                { "forenames": "Lizzie", "surname": "Cowell", "citizenID": "444FF" },
+                { "forenames": "Lizzie", "surname": "Cowell", "citizenID": "567RE" }
+
+
             ],
             headerList: ["citizenID"]
         }
@@ -28,22 +33,39 @@ export default class CitizenList extends Component {
             }
         })
     }
-e
-    handleClick(e){
+    
+    handleClick(e) {
         e.preventDefault();
         console.log(e.target.value);
     }
 
-    render() {
+    compare(a, b) {
+        const citizenA = a.citizenID;
+        const citizenB = b.citizenID;
 
-        return (
-            <div>
-                <p>Please click the citizen you want to view:</p>
-                <table id='tables'>
-                <TableHead headerList={this.state.headerList}></TableHead>
-                <TableBody onClick={this.handleClick} infoList={this.state.citizenList}></TableBody>
-                </table>
-            </div>
-        );
+        let comparison = 0;
+        if (citizenA > citizenB) {
+            comparison = 1;
+        } else if (citizenA < citizenB) {
+            comparison = -1;
+        }
+        return comparison;
     }
+
+render() {
+
+    return (
+        <div id='cardList'>
+            <h4>Please choose a citizen to view more information:</h4>
+            {this.state.citizenList.sort(this.compare).map(citizen =>
+                <Card border="primary" class='citizenCard'>
+                    <Card.Body>
+                        <Card.Title>Citizen ID: {citizen.citizenID}</Card.Title>
+                        <Card.Text>{citizen.forenames} {citizen.surname}</Card.Text>
+                        <button class='cardButton' value={citizen.citizenID} handleClick={this.handleClick}>Submit</button>
+                    </Card.Body>
+                </Card>)}
+        </div>
+    );
+}
 }
