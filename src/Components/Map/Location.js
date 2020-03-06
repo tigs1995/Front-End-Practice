cdd9747b6e03a8225fa2c7dac176359e1216a330
eac-alt <lacolwell@outlook.com>
2020-03-06 13:09:32 +0000

    Basic Map API

--- src/Components/Map/Location.js
+++ src/Components/Map/Location.js
@@ -1,35 +1,83 @@
-import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
- 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
- 
-class Location extends Component {
-  static defaultProps = {
-    center: {
-      lat: 53.4662133,
-      lng: -2.2780334
-    },
-    zoom: 11
-  };
- 
-  render() {
-    return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: "AIzaSyDBxnd19DzbFBaNgtX75EgNx6znWS9pzpY" }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
+import React, { useState, useEffect } from "react";
+import {
+  withGoogleMap,
+  withScriptjs,
+  GoogleMap,
+  Marker,
+  InfoWindow
+} from "react-google-maps";
+import bankData from "./bankData.json";
+import mapStyles from "./mapStyles";
+
+function Map() {
+  const [selectedPark, setSelectedPark] = useState(null);
+
+  useEffect(() => {
+    const listener = e => {
+      if (e.key === "Escape") {
+        setSelectedPark(null);
+      }
+    };
+    window.addEventListener("keydown", listener);
+
+    return () => {
+      window.removeEventListener("keydown", listener);
+    };
+  }, []);
+
+  return (
+    <GoogleMap
+      defaultZoom={10}
+      defaultCenter={{ lat: 53.02478, lng: -2.19952 }}
+      defaultOptions={{ styles: mapStyles }}
+    >
+      {/* {bankData.ATM.map(bank => (
+        <Marker
+          key={bank.ATM_ID}
+          position={{
+            lat: bank.latitude,
+            lng: bank.longitude
+          }}
+          // onClick={() => { */}
+      // setSelectedPark(park);
+      {/* // }}
+          // icon={{
+          //   url: `/skateboarding.svg`,
+          //   scaledSize: new window.google.maps.Size(25, 25)
+          // }}
+        />
+      ))} */}
+      {selectedPark && (
+        <InfoWindow
+          onCloseClick={() => {
+            setSelectedPark(null);
+          }}
+          position={{
+            lat: selectedPark.geometry.coordinates[1],
+            lng: selectedPark.geometry.coordinates[0]
+          }}
         >
-          <AnyReactComponent
-            lat={53.4662133}
-            lng={-2.2780334}
-            text="My location"
-          />
-        </GoogleMapReact>
-      </div>
-    );
-  }
+          <div>
+            <h2>{selectedPark.properties.NAME}</h2>
+            <p>{selectedPark.properties.DESCRIPTIO}</p>
+          </div>
+        </InfoWindow>
+      )}
+    </GoogleMap>
+  );
 }
- 
-export default Location;
+
+const MapWrapped = withScriptjs(withGoogleMap(Map));
+
+export default function App() {
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <MapWrapped
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDBxnd19DzbFBaNgtX75EgNx6znWS9pzpY`}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    </div>
+  );
+}
