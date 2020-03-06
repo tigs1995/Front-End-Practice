ca626a82450c794b001e74080b6d3ce0c0007ec1
tigs1995 <tigsye@hotmail.com>
2020-03-06 15:19:16 +0000

    Vehicle list axios complete and working

--- src/Components/ListPage/VehicleList.js
+++ src/Components/ListPage/VehicleList.js
@@ -1,23 +1,19 @@
 import React, { Component } from "react";
 import { BASE_URL, VEHICLE_LIST } from "../Constants";
 import axios from "axios";
-import { Card, ListItem } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 
 export default class VehicleList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      vehicleList: [
-        { vehicleRegistrationNo: "A" },
-        { vehicleRegistrationNo: "B" }
-      ]
+      vehicleList: []
     };
   }
 
   componentDidMount(props) {
-    const vehicleRegistrationNo = this.props.match.params.reg;
     axios
-      .get(`${BASE_URL}${VEHICLE_LIST}`, { vehicleRegistrationNo })
+      .post(`${BASE_URL}${VEHICLE_LIST}`, { vehicleRegistrationNo: this.props.match.params.reg })
       .then(res => {
         console.log(res);
         this.setState({ vehicleList: res.data });
@@ -27,7 +23,7 @@ export default class VehicleList extends Component {
 
   handleClick(e) {
     e.preventDefault();
-    console.log(e.target.value);
+    
   }
 
   compare(a, b) {
@@ -45,7 +41,6 @@ export default class VehicleList extends Component {
 
   render() {
     const headings = ["Vehicle Registration"];
-
     const rows = this.state.vehicleList.map(vehicle => [
       vehicle.vehicleRegistrationNo
     ]);
@@ -54,14 +49,14 @@ export default class VehicleList extends Component {
       <div id="cardList">
         <h4>Please choose a vehicle to view more information:</h4>
         {this.state.vehicleList.sort(this.compare).map(vehicle => (
-          <Card border="primary" class="vehicleCard">
+          <Card border="primary" className="vehicleCard">
             <Card.Body>
               <Card.Title>Vehicle Registration Number:</Card.Title>
               <Card.Text>{vehicle.vehicleRegistrationNo}</Card.Text>
               <button
-                class="cardButton"
+                className="cardButton"
                 value={vehicle.vehicleRegistrationNo}
-                handleClick={this.handleClick}
+                onClick={this.handleClick}
               >
                 Submit
               </button>
--- src/Components/SearchPages/VehicleSearch.js
+++ src/Components/SearchPages/VehicleSearch.js
@@ -1,45 +1,46 @@
 import React, { Component } from 'react';
 import DataInput from "../DataInput";
 import axios from "axios";
+import { CHECK_EXISTING_VEHICLE, BASE_URL } from "../Constants";
 
-import {CHECK_EXISTING_VEHICLE, BASE_URL} from "../Constants";
+export default class VehicleSearch extends Component {
 
-export default class VehicleSearch extends Component{
-constructor(props){
-    super(props);
-    this.state = {
-        vehicleRegistrationNo: "",
-        errorMessage: ''
+    constructor(props) {
+        super(props);
+        this.state = {
+            vehicleRegistrationNo: '',
+            errorMessage: null
+        }
     }
-}
-
-    handleChange = ({target: {value}}) => {
-        this.setState({vehicleRegistrationNo: value});
-    } 
-
-    handleSubmit = (event) => {
-        event.preventDefault();
-        axios.get(`${BASE_URL}${CHECK_EXISTING_VEHICLE}${this.state.vehicleRegistrationNo}`)
-        .then(response => {
-            if (response.data.Error){
-                this.setState({ errorMessage: response.dataError })
-            }
-            else if (response.data === false){
-                this.setState({ errorMessage: 'Vehicle not found.'});
-            }
-            else{
-                this.props.history.push(`VehicleList/${this.state.vehicleRegistrationNo}`);
-            }
-        })
+    handleChange = ({ target }) => {
+        this.setState({ [target.name]: target.value });
     }
 
-    render(){
-        return(<form onSubmit={this.handSubmit}>
+    handleSubmit = e => {
+        e.preventDefault();
+        axios.post(`${BASE_URL}${CHECK_EXISTING_VEHICLE}`, { vehicleRegistrationNo: this.state.vehicleRegistrationNo })
+            .then(response => {
+                console.log(response)
+                if (response.data.Error) {
+                    this.setState({ errorMessage: response.dataError })
+                }
+                else if (response.data === false) {
+                    this.setState({ errorMessage: 'Vehicle not found.' });
+                }
+                else {
+                    this.props.history.push(`VehicleList/${this.state.vehicleRegistrationNo}`);
+                }
+            })
+    }
+
+    render() {
+        return (
+        <form onSubmit={this.handleSubmit}>
             <DataInput type="text" name="vehicleRegistrationNo" placeholder="Vehicle Registration Number" onChange={this.handleChange}></DataInput>
-            <button type="submit">Search</button>
+            <button>Search</button>
             <span className='error'>{this.state.errorMessage}</span>
         </form>
-            
+
         );
     }
 
