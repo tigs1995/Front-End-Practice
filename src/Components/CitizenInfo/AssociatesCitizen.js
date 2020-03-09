d1e47cdddc87454b1f381291423207214caf3685
eac-alt <lacolwell@outlook.com>
2020-03-09 16:33:46 +0000

    Financial page working with back end

--- src/Components/CitizenInfo/AssociatesCitizen.js
+++ src/Components/CitizenInfo/AssociatesCitizen.js
@@ -1,61 +1,60 @@
-import React from "react";
+import React, { Component } from "react";
 import Styles from "../SortingTable/Styles";
 import SortingTable from "../SortingTable/SortingTable";
-import {BASE_URL, GET_ASSOCIATES, GET_CITIZEN} from "../../config/Constants.json";
+import {
+  BASE_URL,
+  GET_ASSOCIATES,
+  GET_CITIZEN
+} from "../../config/Constants.json";
 import axios from "axios";
 
-export default class AssociatesCitizen extends React.Component{
-    constructor(props){
-        super(props);
-        
-        this.state = {
-            associates: [
-            {  "forenames": "Tigs", "surname": "K", "noOfInteractions": "12"},
-            {  "forenames": "Lizzie", "surname": "C", "noOfInteractions": "11"},
-            {  "forenames": "Alwin", "surname": "T", "noOfInteractions": "16"},
-            {  "forenames": "Jon", "surname": "G", "noOfInteractions": "23"},
-            {  "forenames": "Ewan", "surname": "D", "noOfInteractions": "52"}
-            ],
-            citizenBeingSearched: ""
+export default class AssociatesCitizen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      associates: [],
+      citizenBeingSearched: ""
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ citizenID: this.props.match.params.id });
+    console.log(this.state.citizenID);
+
+    axios
+      .get(`${BASE_URL}${GET_ASSOCIATES}`, {
+        citizenID: this.props.match.params.id
+      })
+      .then(response => {
+        if (response.data.Error) {
+          console.log(response.data.Error);
+        } else {
+          this.setState({ forenames: response.data.forenames });
+          this.setState({ surname: response.data.surname });
         }
-    }
+      });
 
-    componentDidMount() {
-        this.setState({citizenID : this.props.match.params.id});
-        console.log(this.state.citizenID);
+    axios
+      .get(`${BASE_URL}${GET_CITIZEN}${this.state.citizenID}`)
+      .then(response => {
+        if (response.data.Error) {
+          console.log(response.data.Error);
+        } else {
+          this.setState({ forenames: response.data.forenames });
+          this.setState({ surname: response.data.surname });
+        }
+      });
+  }
 
-        
-        axios.get(`${BASE_URL}${GET_ASSOCIATES}${this.state.citizenID}`).then(response => {
-          if (response.data.Error) {
-            console.log(response.data.Error);
-          }
-          else {
-            this.setState({ forenames: response.data.forenames });
-            this.setState({ surname: response.data.surname });
-          }
-        });
-
-
-        axios.get(`${BASE_URL}${GET_CITIZEN}${this.state.citizenID}`).then(response => {
-            if (response.data.Error) {
-              console.log(response.data.Error);
-            }
-            else {
-              this.setState({ forenames: response.data.forenames });
-              this.setState({ surname: response.data.surname });
-            }
-          });
-    }
-    
-       
-
-    render(){
-        return(<div>
-            <p>Associates of: {this.state.citizenBeingSearched}</p>
-            <Styles>
-            <SortingTable data={this.state.associates} />
-            </Styles>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <p>Associates of: {this.state.citizenBeingSearched}</p>
+        <Styles>
+          <SortingTable data={this.state.associates} />
+        </Styles>
+      </div>
+    );
+  }
 }
--- src/Components/CitizenInfo/VehicleCitizen.js
+++ src/Components/CitizenInfo/VehicleCitizen.js
@@ -14,12 +14,12 @@ export default class VehicleCitizen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
       citizenID: "",
       vehicleList: [],
-      ANPRList: []
+      ANPRList: [],
+      vehicleError: "",
+      ANPRError: ""
     };
-
   }
 
   componentWillMount() {
@@ -31,23 +31,29 @@ export default class VehicleCitizen extends Component {
         citizenID: this.props.match.params.id
       })
       .then(response => {
-        this.setState({ vehicleList: response.data.vehicleRegistrations });
-        for (let i = 0; i < this.state.vehicleList.length; i++) {
-          let vehicleReg = this.state.vehicleList[i].vehicleRegistrationNo;
-          axios
-            .post(`${BASE_URL}${GET_ANPR_INFO}`, {
-              vehicleRegistrationNo: vehicleReg
-            })
-            .then(response => {
-              if (response.data.Error) {
-                console.log(response.data.Error);
-              } else {
-                this.setState({ ANPRList: response.data });
-              }
-            })
-            .catch(error => {
-              console.log("Error: " + error);
-            });
+        if (response.data.Warning) {
+          this.setState({ vehicleError: response.data.Warning });
+        } else {
+          this.setState({ vehicleList: response.data.vehicleRegistrations });
+          for (let i = 0; i < this.state.vehicleList.length; i++) {
+            let vehicleReg = this.state.vehicleList[i].vehicleRegistrationNo;
+            axios
+              .post(`${BASE_URL}${GET_ANPR_INFO}`, {
+                vehicleRegistrationNo: vehicleReg
+              })
+              .then(response => {
+                if (response.data.Error) {
+                  console.log(response.data.Error);
+                } else if (response.data.Warning) {
+                  this.setState({ ANPRError: response.data.Warning });
+                } else {
+                  this.setState({ ANPRList: response.data });
+                }
+              })
+              .catch(error => {
+                console.log("Error: " + error);
+              });
+          }
         }
       })
       .catch(error => {
@@ -60,10 +66,12 @@ export default class VehicleCitizen extends Component {
       <div>
         <Styles>
           <h2>Vehicles</h2>
+          <span id="error">{this.state.vehicleError}</span>
           <SortingTable data={this.state.vehicleList} />
         </Styles>
         <Styles>
           <h2>ANPR Information</h2>
+          <span id="error">{this.state.ANPRError}</span>
           <SortingTable data={this.state.ANPRList} />
         </Styles>
       </div>
