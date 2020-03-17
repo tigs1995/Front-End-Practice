6189e1306c3645e1d45fc0374348bed3affc4443
Simone Knowles <tigsye@hotmail.com>
2020-03-17 12:29:41 +0000

    Associates table improved

--- src/Components/CitizenInfo/CountCalls.js
+++ src/Components/CitizenInfo/CountCalls.js
@@ -3,18 +3,23 @@ function CountCalls(arr) {
   let outboundCalls = arr.outboundCallAssociates;
   let citizenArray = [];
   let citizenSet = new Set();
+  let forenamesSet = new Set();
+  let surnameSet = new Set();
   let numOfCalls = [];
   let output = [];
 
   for (let i = 0; i < inboundCalls.length; i++) {
-    let citizen = inboundCalls[i].citizenID;
     citizenArray.push(inboundCalls[i].citizenID);
     citizenSet.add(inboundCalls[i].citizenID);
+    forenamesSet.add(inboundCalls[i].forenames);
+    surnameSet.add(inboundCalls[i].surname);
   }
 
   for (let i = 0; i < outboundCalls.length; i++) {
-    citizenArray.push(inboundCalls[i].citizenID);
-    citizenSet.add(inboundCalls[i].citizenID);
+    citizenArray.push(outboundCalls[i].citizenID);
+    citizenSet.add(outboundCalls[i].citizenID);
+    forenamesSet.add(outboundCalls[i].forenames);
+    surnameSet.add(outboundCalls[i].surname);
   }
 
   for (let i = 0; i < citizenSet.size; i++){
@@ -22,6 +27,8 @@ function CountCalls(arr) {
   }
 
   let citizenArrayNew = Array.from(citizenSet);
+  let forenamesArray = Array.from(forenamesSet);
+  let surnameArray = Array.from(surnameSet);
 
   for (let n = 0; n < citizenArrayNew.length; n++) {
     for (let n = 0; n < citizenArray.length; n++) {
@@ -32,9 +39,11 @@ function CountCalls(arr) {
   }
 
   for(let i = 0; i < citizenArrayNew.length; i++){
-    output.push({"Citizen ID": citizenArrayNew[i], "Number Of Calls": numOfCalls[i]})
+    output.push({"Citizen ID": citizenArrayNew[i], "Fornames": forenamesArray[i], "Surname": surnameArray[i], "Number Of Calls": numOfCalls[i]})
   }
+  
   return output;
+  
 }
 
 export default CountCalls;
